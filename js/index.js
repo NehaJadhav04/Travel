af88e884260cf6c1bffbdf2ec8116d2104bf7cf5
NehaJadhav04 <jadhavneha0412@gmail.com>
2021-10-20 00:00:27 +0530

    Changes made

--- js/index.js
+++ js/index.js
@@ -106,3 +106,46 @@ scroll.reveal(`.service .col,.trip .row`, {
 scroll.reveal(`.trip .title,.more .col:first-child,.newsletter .col`, {
   origin: "left",
 });
+
+const swiper1 = new Swiper(".slider-1", {
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-next",
+    prevEl: ".swiper-prev",
+  },
+});
+
+const swiper2 = new Swiper(".slider-2", {
+  autoHeight: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+const swiper3 = new Swiper(".slider-3", {
+  autoHeight: true,
+  effect: "coverflow",
+  grabCursor: true,
+  loop: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 40,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  navigation: {
+    nextEl: ".custom-next",
+    prevEl: ".custom-prev",
+  },
+  pagination: {
+    el: ".custom-pagination",
+  },
+});
